f8f010906cedf16ebec3947e2e7610e60cb4f0d8
gisul <134128657+vutagwa@users.noreply.github.com>
2024-02-16 13:44:25 +0300

    add all new js changes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function rain(){
     let heart = document.querySelector('.heart');
     let e = document.createElement('div');
-    e.classList('drop');
+    e.classList.add('drop');
     heart.appendChild(e);
     let left = math.floor(matht )
     setTimeout(function(){
         heart.removeChild(e)
     },5000)
+    setInterval(function(){
+        rain()
+    },50)
 }
